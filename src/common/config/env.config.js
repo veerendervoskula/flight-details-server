e3fecff0e81963d29a7f100292171d625653bd9f
veer <voskulaveerender@gmail.com>
2021-01-25 00:14:45 +0000

    updated env config

--- src/common/config/env.config.js
+++ src/common/config/env.config.js
@@ -3,14 +3,14 @@
  * Default values are kept here. This will be overridden with process.env
  */
 module.exports = {
-    ...{
-        PORT : 3600,
-        NODE_ENV: 'development',
-        DB_HOST : 'mongodb+srv',
-        DB_USER : 'veerender',
-        DB_PASS : 'Jissu@04',
-        DB_CLUSTER : 'flights.ioohf.mongodb.net/veerender'        
-    },
-    ...process.env
-}
-//mongodb+srv://veerender:<password>@flights.ioohf.mongodb.net/<dbname>?retryWrites=true&w=majority
+  ...{
+    PORT: 3600,
+    NODE_ENV: 'development',
+    DB_HOST: 'mongodb+srv',
+    DB_USER: 'veerender',
+    DB_PASS: 'JXXXXX',
+    DB_CLUSTER: 'flights.ioohf.mongodb.net/veerender',
+  },
+  ...process.env,
+};
+// mongodb+srv://veerender:<password>@flights.ioohf.mongodb.net/<dbname>?retryWrites=true&w=majority
